f3134b08bb58dd33a060705e839694b2c11958c3
Steve Ash <stevemash@gmail.com>
2015-04-02 18:46:35 -0500

    redoing the cascading stuff to be smarter

---
+++ jg2p-core/src/main/java/com/github/steveash/jg2p/train/CascadeEncoder.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015 Steve Ash
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.steveash.jg2p.train;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import com.github.steveash.jg2p.PhoneticEncoder;
+import com.github.steveash.jg2p.Word;
+import com.github.steveash.jg2p.align.AlignModel;
+import com.github.steveash.jg2p.seqbin.SeqBinModel;
+
+import java.io.Serializable;
+import java.util.List;
+
+import cc.mallet.types.Labeling;
+
+/**
+ * @author Steve Ash
+ */
+public class CascadeEncoder implements Serializable {
+
+  private static final long serialVersionID = 123L;
+
+  private final AlignModel alignModel;
+  private final PhoneticEncoder encoderG;
+  private final PhoneticEncoder encoderB;
+  private final SeqBinModel seqBin;
+
+  public CascadeEncoder(AlignModel alignModel, PhoneticEncoder encoderG, PhoneticEncoder encoderB, SeqBinModel seqBin) {
+    this.alignModel = alignModel;
+    this.encoderG = encoderG;
+    this.encoderB = encoderB;
+    this.seqBin = seqBin;
+  }
+
+  public List<PhoneticEncoder.Encoding> encode(String word) {
+    Word input = Word.fromNormalString(word);
+    return encode(input);
+  }
+
+  public List<PhoneticEncoder.Encoding> encode(Word input) {
+
+    Labeling gOrB = seqBin.classify(input);
+    List<PhoneticEncoder.Encoding> resultsG = encoderG.encode(input);
+    List<PhoneticEncoder.Encoding> resultsB = encoderB.encode(input);
+    double gProb, bProb;
+    boolean firstG = ((String)gOrB.getBestLabel().getEntry()).equalsIgnoreCase("G");
+    if (firstG) {
+      gProb = gOrB.getBestValue();
+      bProb = 1.0 - gProb;
+    } else {
+      bProb = gOrB.getBestValue();
+      gProb = 1.0 - bProb;
+    }
+    PhoneticEncoder.Encoding best = null;
+    double bestProb = Double.NEGATIVE_INFINITY;
+    for (PhoneticEncoder.Encoding encoding : resultsG) {
+      double thisProb = encoding.tagProbability() * gProb;
+      if (thisProb > bestProb) {
+        bestProb = thisProb;
+        best = encoding;
+      }
+    }
+    for (PhoneticEncoder.Encoding encoding : resultsB) {
+      double thisProb = encoding.tagProbability() * bProb;
+      if (thisProb > bestProb) {
+        bestProb = thisProb;
+        best = encoding;
+      }
+    }
+    return ImmutableList.of(best);
+  }
+}
--- jg2p-core/src/test/groovy/PsaurusCascadeEval.groovy
+++ jg2p-core/src/test/groovy/PsaurusCascadeEval.groovy
@@ -15,12 +15,19 @@
  */
 
 import com.github.steveash.jg2p.align.InputReader
+import com.github.steveash.jg2p.align.InputRecord
 import com.github.steveash.jg2p.align.Maximizer
 import com.github.steveash.jg2p.align.TrainOptions
+import com.github.steveash.jg2p.train.CascadingTrainer
 import com.github.steveash.jg2p.train.JointEncoderTrainer
 import com.github.steveash.jg2p.train.SimpleEncoderTrainer
+import com.github.steveash.jg2p.util.ListEditDistance
+import com.github.steveash.jg2p.util.Percent
 import com.github.steveash.jg2p.util.ReadWrite
+import groovyx.gpars.GParsPool
 import org.slf4j.LoggerFactory
+
+import java.util.concurrent.atomic.AtomicLong
 
 /**
  * Driver for the whole end to end training and eval process so that I can play with the alignment code to improve
@@ -30,8 +37,6 @@ import org.slf4j.LoggerFactory
 def trainFile = "g014b2b-results.train"
 def testFile = "g014b2b.test"
 def train = InputReader.makeDefaultFormatReader().readFromClasspath(trainFile)
-def trainG = train.findAll {it.memo == "G"}
-def trainB = train.findAll {it.memo == "B"}
 
 def test = InputReader.makePSaurusReader().readFromClasspath(testFile)
 def opts = new TrainOptions()
@@ -43,22 +48,47 @@ opts.useWindowWalker = true
 opts.includeXEpsilons = true 
 opts.maximizer = Maximizer.JOINT
 opts.topKAlignCandidates = 1
-opts.minAlignScore = -150
+opts.minAlignScore = Integer.MIN_VALUE;
 opts.initCrfFromModelFile = "../resources/psaur_22_xEps_ww_f3.dat"
+
+def gFile = new File("../resources/psaur_22_xEps_ww_GB_G1.dat")
+def bFile = new File("../resources/psaur_22_xEps_ww_GB_B1.dat")
+def sbFile = new File("../resources/cmu_gb_seqbin_A.dat")
+assert gFile.exists()
+assert bFile.exists()
+assert sbFile.exists()
 
 def log = LoggerFactory.getLogger("psaurus")
 log.info("Starting training with $trainFile and $testFile with opts $opts")
 
-log.info(" -- training the whole file first " + train.size())
-def t = new SimpleEncoderTrainer()
-//def t = new JointEncoderTrainer()
-def modelG = t.trainAndEval(train, null, opts)
-ReadWrite.writeTo(modelG, new File("../resources/psaur_22_xEps_ww_GB_G2.dat"))
+log.info("Training everything ...")
+def t = new CascadingTrainer()
+def ce = t.train(train, opts, gFile, bFile, sbFile)
+ReadWrite.writeTo(ce, new File("../resources/psaur_22_xEps_ww_CE_A.dat"))
 
-log.info(" -- training the B file next " + trainB.size())
-def t2 = new SimpleEncoderTrainer()
-//def t = new JointEncoderTrainer()
-def modelB = t2.trainAndEval(trainB, null, opts)
-ReadWrite.writeTo(modelB, new File("../resources/psaur_22_xEps_ww_GB_B2.dat"))
+AtomicLong wordTotal = new AtomicLong(0)
+AtomicLong wordGood = new AtomicLong(0)
+AtomicLong phoneTotal = new AtomicLong(0)
+AtomicLong phoneGood = new AtomicLong(0)
+
+GParsPool.withPool {
+  test.everyParallel { InputRecord input ->
+    def best = ce.encode(input.left)
+    def pp = best.first()
+
+    // phone compare
+    def edits = ListEditDistance.editDistance(pp.phones, input.yWord.value, 100);
+
+    if (pp.phones == input.yWord.value) {
+      wordGood.incrementAndGet()
+    }
+    wordTotal.incrementAndGet()
+
+    phoneTotal.addAndGet(pp.phones.size())
+    phoneGood.addAndGet(pp.phones.size() - edits)
+  }
+}
+log.info("Good words " + wordGood.get() + " of " + wordTotal.get() + " " + Percent.print(wordGood.get(), wordTotal.get()))
+log.info("Good phones " + phoneGood.get() + " of " + phoneTotal.get() + " " + Percent.print(phoneGood.get(), phoneTotal.get()))
 
 log.info("***********************************Finished*************************************")

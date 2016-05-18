09c1216511c5df7455599c4473094d668ddde611
Steve Ash <stevemash@gmail.com>
2016-05-17 21:20:16 -0500

    sanity check run without 3x2, without penalizer on cmu7

--- jg2p-core/src/test/groovy/PipelineTrain.groovy
+++ jg2p-core/src/test/groovy/PipelineTrain.groovy
@@ -13,13 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import com.github.steveash.jg2p.PipelineEncoder
 import com.github.steveash.jg2p.PipelineModel
 import com.github.steveash.jg2p.align.InputReader
 import com.github.steveash.jg2p.align.TrainOptions
+import com.github.steveash.jg2p.eval.BulkEval
+import com.github.steveash.jg2p.eval.EvalPrinter
 import com.github.steveash.jg2p.train.PipelineTrainer
 import com.github.steveash.jg2p.util.GroovyLogger
 import com.github.steveash.jg2p.util.ReadWrite
 import com.google.common.base.Stopwatch
+import org.apache.commons.lang3.time.DateFormatUtils
 import org.slf4j.LoggerFactory
 
 /**
@@ -28,16 +33,18 @@ import org.slf4j.LoggerFactory
  */
 //def inputFile = "g014b2b.train.syll"
 def inputFile = "cmu7b.train"
+def testFile = "cmu7b.test"
 //def inputFile = "cmudict.2kA.txt"
 //def inputFile = "g014b2b.test"
 def inputs = InputReader.makePSaurusReader().readFromClasspath(inputFile)
 //def inputs = InputReader.makeDefaultFormatReader().readFromClasspath(inputFile)
 
+def testImmediately = true
 def opts = new TrainOptions()
-opts.maxXGram = 4
-opts.maxYGram = 3
-opts.onlyOneGrams = false
-opts.useCityBlockPenalty = true
+opts.maxXGram = 2
+opts.maxYGram = 2
+opts.onlyOneGrams = true
+opts.useCityBlockPenalty = false
 opts.useWindowWalker = true
 opts.useSyllableTagger = true
 opts.maxPronouncerTrainingIterations = 400
@@ -57,7 +64,8 @@ opts.trainTrainingAligner = false
 //opts.useInputRerankExampleCsv = "../resources/pip_rre.csv"
 //opts.trainReranker = true
 //opts.writeOutputRerankExampleCsv = "../resources/rerank_inputs.csv"
-def outFile = "../resources/pipe_43sy_cmu7_F11_5a.dat"
+def outFile = "../resources/pipe_22sy_nocityblk_cmu7_F11_1.dat"
+
 
 def log = LoggerFactory.getLogger("psaurus")
 out = new GroovyLogger(log)
@@ -77,3 +85,17 @@ watch.stop()
 println "Wrote to $outFile"
 println "Entire training process took $watch"
 log.info("***********************************Finished*************************************")
+
+if (testImmediately) {
+  println "testing right now..."
+  def test = InputReader.makePSaurusReader().readFromClasspath(testFile)
+  pm.makeSparse()
+  def enc = new PipelineEncoder(pm)
+
+  println "Starting eval with encoder $enc"
+  def eval = new BulkEval(enc)
+  def stats = eval.groupAndEval(test)
+//  def ts = DateFormatUtils.format(new Date(), "yyyyMMddHHmmss")
+//  EvalPrinter.writeExamples(new File("target/badexamples-${ts}.txt"), stats)
+  EvalPrinter.printTo(out, stats, outFile)
+}

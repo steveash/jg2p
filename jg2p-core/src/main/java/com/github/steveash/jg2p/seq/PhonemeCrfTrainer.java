65c55e6660320425b17cf7d149fd686b48e741db
Steve Ash <stevemash@gmail.com>
2016-05-18 22:57:28 -0500

    trying syll count feature again

--- jg2p-core/src/main/java/com/github/steveash/jg2p/seq/PhonemeCrfTrainer.java
+++ jg2p-core/src/main/java/com/github/steveash/jg2p/seq/PhonemeCrfTrainer.java
@@ -251,7 +251,7 @@ public class PhonemeCrfTrainer {
     LabelAlphabet labelAlpha = (LabelAlphabet) labelPipe.getTargetAlphabet();
 
     return new SerialPipes(ImmutableList.of(
-        new AlignmentToTokenSequence(alpha, labelAlpha, true, true, false),   // convert to token sequence
+        new AlignmentToTokenSequence(alpha, labelAlpha, true, true, true),   // convert to token sequence
         new TokenSequenceLowercase(),                       // make all lowercase
         new NeighborTokenFeature(true, makeNeighbors()),         // grab neighboring graphemes
         new NeighborShapeFeature(true, makeShapeNeighs()),
